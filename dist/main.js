025cdb27cffa3b126d4aefc8a7acf3b030f4fa1b
Abdulrahim <abdulrhimelsayed@gmail.com>
2026-05-29 18:47:56 +0300

    learn data type

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log(10);
+let name = "abdo";
+let num = 10;
+let hire = true;
+let all = "Abdulrahim";
+all = 100;
+function ADD(n1, n2) {
+    return n1 + n2;
+}
+console.log(ADD(10, 20));
+console.log(typeof (ADD(10, 20)));
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log(10);
+let name :string="abdo";
+let num :number = 10;
+let hire :boolean= true; 
+let all :any ="Abdulrahim";
+all =100;
+
+function ADD(n1:number,n2:number){
+    return n1+n2;
+}
+
+console.log(ADD(10,20));
+console.log(typeof(ADD(10,20)));
