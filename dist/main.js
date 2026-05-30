759c0682fb25a190a1d1d664de81a4abc4b0bda5
Abdulrahim <abdulrhimelsayed@gmail.com>
2026-05-30 15:56:38 +0300

    function return

--- dist/main.js
+++ dist/main.js
@@ -1,13 +1,20 @@
 "use strict";
+// let name :string="abdo";
+// let num :number = 10;
+// let hire :boolean= true; 
+// let all :any ="Abdulrahim";
+// all =100;
 Object.defineProperty(exports, "__esModule", { value: true });
-let name = "abdo";
-let num = 10;
-let hire = true;
-let all = "Abdulrahim";
-all = 100;
-function ADD(n1, n2) {
-    return n1 + n2;
+// function ADD(n1:number,n2:number){
+//     return n1+n2;
+// }
+// console.log(ADD(10,20));
+// console.log(typeof(ADD(10,20)));
+let showMsg = true;
+function showDaitils(name, age, salary) {
+    if (showMsg) {
+        return `Hello ${name} , Age is ${age} , Salary is ${salary}`;
+    }
 }
-console.log(ADD(10, 20));
-console.log(typeof (ADD(10, 20)));
+console.log("abdo", 21, 15);
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
-let name :string="abdo";
-let num :number = 10;
-let hire :boolean= true; 
-let all :any ="Abdulrahim";
-all =100;
+// let name :string="abdo";
+// let num :number = 10;
+// let hire :boolean= true; 
+// let all :any ="Abdulrahim";
+// all =100;
 
-function ADD(n1:number,n2:number){
-    return n1+n2;
+// function ADD(n1:number,n2:number){
+//     return n1+n2;
+// }
+
+// console.log(ADD(10,20));
+// console.log(typeof(ADD(10,20)));
+
+let showMsg=true;
+function showDaitils(name:string,age:number,salary:number){
+    if(showMsg){
+        return `Hello ${name} , Age is ${age} , Salary is ${salary}`;
+    }
 }
 
-console.log(ADD(10,20));
-console.log(typeof(ADD(10,20)));
+console.log("abdo",21,15);
