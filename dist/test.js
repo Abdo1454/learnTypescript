b549ac3e36908e37d4a5776bfaaa941fdbf1378f
Abdulrahim <abdulrhimelsayed@gmail.com>
2026-06-03 14:16:49 +0300

    enums is very simple

--- dist/test.js
+++ dist/test.js
@@ -1,4 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log(Math.round(10.3));
+// Enums
+const KIDS = 15;
+const EASY = 9;
+const MEDIUM = 6;
+const HARD = 3;
+var Level;
+(function (Level) {
+    Level[Level["Kids"] = 15] = "Kids";
+    Level[Level["Easy"] = 9] = "Easy";
+    Level[Level["Medium"] = 6] = "Medium";
+    Level[Level["Hard"] = 3] = "Hard";
+})(Level || (Level = {}));
+let lv1 = "Easy";
+if (lv1 == "Easy") {
+    console.log(`the level is ${lv1} and the time is ${Level.Easy} `);
+}
 //# sourceMappingURL=test.js.map
--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,15 @@
-console.log(Math.round(10.3));
+// Enums
+const KIDS =15;
+const EASY =9;
+const MEDIUM =6;
+const HARD =3;
+enum Level{
+    Kids=15,
+    Easy=9,
+    Medium=6,
+    Hard=3
+}
+let lv1:string="Easy";
+if(lv1=="Easy"){
+    console.log(`the level is ${lv1} and the time is ${Level.Easy} `);
+} 
