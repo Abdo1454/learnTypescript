99bc88c9fbb3215602d2ba10018860600afb484a
Abdulrahim <abdulrhimelsayed@gmail.com>
2026-06-04 23:42:11 +0300

    enums part two

--- dist/test.js
+++ dist/test.js
@@ -5,9 +5,15 @@ const KIDS = 15;
 const EASY = 9;
 const MEDIUM = 6;
 const HARD = 3;
+var kids;
+(function (kids) {
+    kids[kids["five"] = 25] = "five";
+    kids[kids["seven"] = 15] = "seven";
+    kids[kids["ten"] = 10] = "ten";
+})(kids || (kids = {}));
 var Level;
 (function (Level) {
-    Level[Level["Kids"] = 15] = "Kids";
+    Level[Level["Kid"] = 25] = "Kid";
     Level[Level["Easy"] = 9] = "Easy";
     Level[Level["Medium"] = 6] = "Medium";
     Level[Level["Hard"] = 3] = "Hard";
@@ -16,4 +22,5 @@ let lv1 = "Easy";
 if (lv1 == "Easy") {
     console.log(`the level is ${lv1} and the time is ${Level.Easy} `);
 }
+console.log(Level.Kid);
 //# sourceMappingURL=test.js.map
--- src/test.ts
+++ src/test.ts
@@ -3,8 +3,14 @@ const KIDS =15;
 const EASY =9;
 const MEDIUM =6;
 const HARD =3;
+enum kids{
+    five=25,
+    seven=15,
+    ten=10
+}
+
 enum Level{
-    Kids=15,
+    Kid=kids.five,
     Easy=9,
     Medium=6,
     Hard=3
@@ -13,3 +19,4 @@ let lv1:string="Easy";
 if(lv1=="Easy"){
     console.log(`the level is ${lv1} and the time is ${Level.Easy} `);
 } 
+console.log(Level.Kid);
